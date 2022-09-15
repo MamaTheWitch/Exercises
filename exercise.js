afab914b758b79aa55d8a21eb9172a71417d2920
MamaTheWitch <104829193+MamaTheWitch@users.noreply.github.com>
2022-09-15 11:13:35 +0300

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -1,3 +1,9 @@
 let phoneNumber = "04567838999";
 let result = phoneNumber.startsWith("+"); 
 console.log(result)
+
+let Name = "thisIsMyUnitedStatesOfWhatever";
+ let lowerCase = Name.toLowerCase();
+ console.log(lowerCase)
+ let capitalizedFirst = lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
+ console.log(capitalizedFirst)
