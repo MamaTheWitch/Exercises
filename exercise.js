ba63ee6d4884bc177cb67a3f4998aa1592479842
MamaTheWitch <104829193+MamaTheWitch@users.noreply.github.com>
2022-09-20 14:02:17 +0300

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -1,14 +1,14 @@
 //Create an if statement that tells if a phone number is in international format (starts with a plus character)
 let phoneNumber = "04567838999";
-console.log(phoneNumber.startsWith("+");)
+console.log(phoneNumber.startsWith("+"))
 
 //Create a function that takes a name and converts it to capitalized form (first letter uppercase, rest lowercase)
 let Name = "thisIsMyUnitedStatesOfWhatever";
 let lowerCase = Name.toLowerCase();
-console.log(lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);)
+console.log(lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1))
 
 //Create a function that takes a newspaper heading and converts it to capitalized form (first letter of each word in uppercase)
 let heading = 'why does it always rain on me';
 let words = heading.split(" ");
 for (var i = 0; i < words.length; i++){words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);}
-console.log(words.join(" "));
+console.log(words.join(" "))
