bcc42969448980b2296ae518fbb7104a948bed4e
MamaTheWitch <104829193+MamaTheWitch@users.noreply.github.com>
2022-09-20 13:55:28 +0300

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -11,15 +11,7 @@ let capitalizedFirst = lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
 console.log(capitalizedFirst)
 
 //Create a function that takes a newspaper heading and converts it to capitalized form (first letter of each word in uppercase)
-function capitalize(input) {
-let words = input.split(' ');
-let CapitalizedWords = [];
-words.forEach(element => {CapitalizedWords.push(element[0].toUpperCase() + element.slice(1, element.length));});
-return CapitalizedWords.join(' ');}
-console.log(capitalize("why does it always rain on me"))
-
-const str = 'why does it always rain on me';
-const arr = str.split(" ");
-for (var i = 0; i < arr.length; i++) {arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);}
-const str2 = arr.join(" ");
-console.log(str2);
+let heading = 'why does it always rain on me';
+let words = heading.split(" ");
+for (var i = 0; i < words.length; i++){words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);}
+console.log(words.join(" "));
