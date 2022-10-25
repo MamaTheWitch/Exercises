e6320f7b9cb8b529ef097a7689afe592dc1124e8
MamaTheWitch <sales@mamaknowsbest.fi>
2022-10-25 14:11:02 +0300

    Define JS function bingo

--- whatever.js
+++ whatever.js
@@ -3,3 +3,11 @@ function Whatever() {
 return console.log(myArray)
 };
 Whatever()
+
+function Bingo(x) {
+    if (x == 5) {console.log("BINGO!!!")}
+    else console.log("try again later...")
+  };
+  Bingo(4);
+  Bingo(5);
+  Bingo(6);
