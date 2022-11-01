7782d831e5f0670809349d8486c205a389ea30e1
MamaTheWitch <104829193+MamaTheWitch@users.noreply.github.com>
2022-11-01 11:41:53 +0200

    Update whatever.js

--- whatever.js
+++ whatever.js
@@ -1,13 +1,13 @@
-function Whatever() {
+function makeAstringOfWhatever() {
     let myArray = ["one", " two", " guess what?", " three!"]
 return console.log(String(myArray))
 };
-Whatever()
+makeAstringOfWhatever()
 
-function Bingo(x) {
+function checkBingo(x) {
     if (x > 5) {console.log("BINGO!!!")}
     else console.log("try again later...")
   };
-  Bingo(4);
-  Bingo(5);
-  Bingo(6);
+  checkBingo(4);
+  checkBingo(5);
+  checkBingo(6);
